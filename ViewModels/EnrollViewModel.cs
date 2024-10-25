bba0e8af28fa4fb58d69b9939af874f55295a846
AgnibhaChowdhury <agnibhaabdhi04@gmail.com>
2024-10-25 11:04:34 +0600

    Fixing the .sln problem and Updating this to GitHub.

--- ViewModels/EnrollViewModel.cs
+++
@@ -1,7 +0,0 @@
-using tennis.Models;
-
-public class EnrollViewModel
-{
-    public IEnumerable<Schedule> Schedules { get; set; }
-    public List<int> EnrolledScheduleIds { get; set; }
-}
--- ViewModels/ScheduleViewModel.cs
+++
@@ -1,9 +0,0 @@
-public class ScheduleViewModel
-{
-    public int ScheduleId { get; set; }
-    public string Name { get; set; }
-    public string Location { get; set; }
-    public DateTime Date { get; set; }
-    public string CoachName { get; set; }
-    public int CoachId { get; set; }
-}
